50c8b8415cbbff0937da22a8c5e4dc603e96ec4b
Ayush <ydvayush27@gmail.com>
2026-08-20 12:20:07 +0530

    Add files via upload

---
+++ dist/index.cjs
@@ -0,0 +1,49 @@
+"use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+
+// server/index.ts
+var import_express = __toESM(require("express"), 1);
+var import_http = require("http");
+var import_path = __toESM(require("path"), 1);
+var import_url = require("url");
+var import_meta = {};
+var __filename = (0, import_url.fileURLToPath)(import_meta.url);
+var __dirname = import_path.default.dirname(__filename);
+async function startServer() {
+  const app = (0, import_express.default)();
+  const server = (0, import_http.createServer)(app);
+  const staticPath = process.env.NODE_ENV === "production" ? import_path.default.resolve(__dirname, "public") : import_path.default.resolve(__dirname, "..", "dist", "public");
+  app.use(import_express.default.static(staticPath));
+  app.get("/manus-storage/*", (req, res) => {
+    res.redirect(307, `https://moonknight-qtedq2zx.manus.space${req.originalUrl}`);
+  });
+  app.get("*", (_req, res) => {
+    res.sendFile(import_path.default.join(staticPath, "index.html"));
+  });
+  const port = process.env.PORT || 3e3;
+  server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}/`);
+  });
+}
+startServer().catch(console.error);
